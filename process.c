7f7bfb0eddb22297683feb6312aa3b9803727d3d
Hebert Clayton <jch223@zoo.cs.yale.edu>
2018-12-02 17:26:53 -0500

    basic commands seem to work

--- process.c
+++ process.c
@@ -1,80 +1,93 @@
 #include "/c/cs323/Hwk5/process-stub.h"
 
-int debugPrint=1;
+int debugPrint=0;
 typedef struct output{
 int value;
 token *tok;
 }output;
 
-char * concatenateArguments(token *tok)
-{
-
-int argLength=0;
-token *origTok=tok;
-while(tok[0].type==ARG)
-{
-	// the plus one account for the space between each argument
-	argLength+=strlen(tok[0].text)+1;
-	tok++;
-}
-char *args=0;
-if(argLength!=0)
-{
-	char *args=malloc(sizeof(char)*(argLength)); args[0]='\0';
-	while(origTok[0].type==ARG)
-	{
-		sprintf(args+strlen(args), origTok[0].text);
-		if(origTok!=tok-1){
-		sprintf(args+strlen(args), " ");
-		}
-		origTok++;
-	}
-	args[argLength-1]='\0';
-}
-return args;
-}
 
 struct output process_pipeline(token *tok)
 {
-struct output out;
-while(tok[0].type==PIPE || tok[0].type==ARG)
-{
-	char *args = concatenateArguments(tok);
-	if(debugPrint){printf("Concatenated Arguments: %s\n", args);}
-	while(tok[0].type==ARG){tok++;}
-	while(tok[0].type==LOCAL)
+	struct output out;
+	while(tok[0].type==PIPE || tok[0].type==ARG)
 	{
-		tok++;
-	}
-	while(RED_OP(tok[0].type))
-	{
-		tok++;
-	}
-	if(args){free(args);}
-	if(tok[0].type==PAR_LEFT)
-	{
-	    tok++;
-	    out.value=process(tok);
-	    int leftParen=0;
-	    while(!(tok[0].type==PAR_RIGHT  && leftParen==0))
-	    {
+		token *origTok=tok;
+		int numArgs=0;
+		while(tok[0].type==ARG){
+			numArgs++;
+			tok++;}
+		char *argArr[numArgs+1];
+		tok=origTok;
+		int i=0;
+		while(tok[0].type==ARG){
+		    argArr[i]=tok[0].text;
+		    tok++;
+		    i++;
+		}
+		//printing out argument list
+		if(debugPrint)
+		{
+			printf("Args: ");
+			for(int x=0; x<numArgs; x++)
+			{
+				printf("%s ", argArr[x]);
+			}
+			printf("\n");
+		}
+		//setting any locals
+		while(tok[0].type==LOCAL)
+		{
+			tok++;
+		}
+		//setting any redirections
+		while(RED_OP(tok[0].type))
+		{
+			tok++;
+		}
+		pid_t pid;
+		//if their were any arguments
+		if(numArgs!=0)
+		{
+			//have to add terminating 0 to list of arguments
+			argArr[numArgs]=0;
+			pid=fork();
+			if(pid==0)
+			{
+			    execvp(argArr[0], argArr);	
+			}
+			else
+			{
+			    int status;
+			    waitpid(pid, &status, 0);
+			    out.value=status;
+			}
+		}
+		//if not we know that there has to be a subcommand
 		if(tok[0].type==PAR_LEFT)
 		{
-		leftParen++;
+		    tok++;
+		    out.value=process(tok);
+		    int leftParen=0;
+		    while(!(tok[0].type==PAR_RIGHT  && leftParen==0))
+		    {
+			if(tok[0].type==PAR_LEFT)
+			{
+			leftParen++;
+			}
+			else if(tok[0].type==PAR_RIGHT)
+			{
+			leftParen--;
+			}
+			tok++;
+		    }
+		    //increment it one more time so we get past the right parenthesis
+		    tok++;
 		}
-		else if(tok[0].type==PAR_RIGHT)
-		{
-		leftParen--;
-		}
-		tok++;
-	    }
-	    //increment it one more time so we get past the right parenthesis
-	    tok++;
+		out.tok=tok;
 	}
-	out.tok=tok;
-}
 
-return out;
+	return out;
 }
 
 int process_and_or(token *tok)
